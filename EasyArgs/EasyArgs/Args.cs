25edefe53cde3f40af2c5f9431d71aaa33ca6fd9
Jeff Linton <jslinton44@gmail.com>
2017-06-09 13:09:05 -0600

    Clean up

--- EasyArgs/EasyArgs/Args.cs
+++ EasyArgs/EasyArgs/Args.cs
@@ -1,7 +1,6 @@
 ﻿using EasyArgs.Models;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Text;
 using System.Text.RegularExpressions;
 
--- EasyArgs/EasyArgs/Models/Argument.cs
+++ EasyArgs/EasyArgs/Models/Argument.cs
@@ -20,82 +20,82 @@ namespace EasyArgs.Models
 
         public static implicit operator byte(Argument arg)
         {
-            return GetDefaultOrParse<byte>(arg, () => byte.Parse(arg.Value));
+            return GetDefaultOrParse(arg, () => byte.Parse(arg.Value));
         }
 
         public static implicit operator sbyte(Argument arg)
         {
-            return GetDefaultOrParse<sbyte>(arg, () => sbyte.Parse(arg.Value));
+            return GetDefaultOrParse(arg, () => sbyte.Parse(arg.Value));
         }
 
         public static implicit operator int(Argument arg)
         {
-            return GetDefaultOrParse<int>(arg, () => int.Parse(arg.Value));
+            return GetDefaultOrParse(arg, () => int.Parse(arg.Value));
         }
 
         public static implicit operator uint(Argument arg)
         {
-            return GetDefaultOrParse<uint>(arg, () => uint.Parse(arg.Value));
+            return GetDefaultOrParse(arg, () => uint.Parse(arg.Value));
         }
 
         public static implicit operator short(Argument arg)
         {
-            return GetDefaultOrParse<short>(arg, () => short.Parse(arg.Value));
+            return GetDefaultOrParse(arg, () => short.Parse(arg.Value));
         }
 
         public static implicit operator ushort(Argument arg)
         {
-            return GetDefaultOrParse<ushort>(arg, () => ushort.Parse(arg.Value));
+            return GetDefaultOrParse(arg, () => ushort.Parse(arg.Value));
         }
 
         public static implicit operator long(Argument arg)
         {
-            return GetDefaultOrParse<long>(arg, () => long.Parse(arg.Value));
+            return GetDefaultOrParse(arg, () => long.Parse(arg.Value));
         }
 
         public static implicit operator ulong(Argument arg)
         {
-            return GetDefaultOrParse<ulong>(arg, () => ulong.Parse(arg.Value));
+            return GetDefaultOrParse(arg, () => ulong.Parse(arg.Value));
         }
 
         public static implicit operator float(Argument arg)
         {
-            return GetDefaultOrParse<float>(arg, () => float.Parse(arg.Value));
+            return GetDefaultOrParse(arg, () => float.Parse(arg.Value));
         }
 
         public static implicit operator double(Argument arg)
         {
-            return GetDefaultOrParse<double>(arg, () => double.Parse(arg.Value));
+            return GetDefaultOrParse(arg, () => double.Parse(arg.Value));
         }
 
         public static implicit operator char(Argument arg)
         {
-            return GetDefaultOrParse<char>(arg, () => char.Parse(arg.Value));
+            return GetDefaultOrParse(arg, () => char.Parse(arg.Value));
         }
 
         public static implicit operator bool(Argument arg)
         {
-            return GetDefaultOrParse<bool>(arg, () => bool.Parse(arg.Value));
+            return GetDefaultOrParse(arg, () => bool.Parse(arg.Value));
         }
 
         public static implicit operator string(Argument arg)
         {
-            return GetDefaultOrParse<string>(arg, () => arg.Value);
+            return GetDefaultOrParse(arg, () => arg.Value);
         }
 
         public static implicit operator decimal(Argument arg)
         {
-            return GetDefaultOrParse<decimal>(arg, () => decimal.Parse(arg.Value));
+            return GetDefaultOrParse(arg, () => decimal.Parse(arg.Value));
         }
 
         public static implicit operator DateTime(Argument arg)
         {
-            return GetDefaultOrParse<DateTime>(arg, () => DateTime.Parse(arg.Value));
+            return GetDefaultOrParse(arg, () => DateTime.Parse(arg.Value));
         }
 
         public static implicit operator Guid(Argument arg)
         {
-            return GetDefaultOrParse<Guid>(arg, () => Guid.Parse(arg.Value));
+            return GetDefaultOrParse(arg, () => Guid.Parse(arg.Value));
         }
     }
 }
